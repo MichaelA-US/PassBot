d1965bd777521c00482732ad3712d0e3913fff10
Michael Ades <MichaelAdes.us@gmail.com>
2023-03-15 20:13:28 -0500

    Add

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import discord
 import os
-from dotenv import load_dotenv
-load_dotenv()
-TOKEN = os.getenv('DISORD_TOKEN')
-client = discord.Client()
+from dotenv import load_dotenv #Prevent's sharing secrets ex. (Discord_Token)
+intents = discord.Intents.default()
+load_dotenv("bot.env")
+TOKEN = os.getenv("DISCORD_TOKEN")
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-
 client.run(TOKEN)
