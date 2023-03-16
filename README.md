d473a4ae6681ae18355e7cc936f3081f7edafffa
Michael Ades <MichaelAdes.us@gmail.com>
2023-03-15 23:15:21 -0500

    Fixed .gitignore for .env files and .txt

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Passbot
+# TO DO: Add encryption
+# Add sql database (sqllite)
+# Add context to password for what it's for
+# Create user DM vs. sending message ID in server and give private key
--- config.py
+++ config.py
@@ -1,11 +1,35 @@
 import discord
 import os
-from dotenv import load_dotenv #Prevent's sharing secrets ex. (Discord_Token)
+from dotenv import load_dotenv #Prevent's sharing secrets ex. (DISCORD_TOKEN)
 intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
 load_dotenv("bot.env")
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("Hello"):
+        await message.channel.send("Hey!")
+        print("Hello sent!")
+    if message.content.startswith("Pass"):
+        f = open("passwords.txt", "a")
+        msg = message.content.replace("Pass","")
+        f.write(msg + " " + str(message.id) + "\n")
+        await message.channel.send("Your message ID: " + str(message.id))
+    if message.content.startswith("Retrieve"):
+        password = ""
+        msg = message.content.replace("Retrieve","")
+        print(msg)
+        f = open("passwords.txt", "r")
+        for line in f:
+            if msg in line:
+                print(line)
+                password = line
+        await message.channel.send(password.replace(msg, ""))
 client.run(TOKEN)
