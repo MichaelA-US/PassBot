d59e09daea7225639eaeec2a48c95fd0f8890575
Michael Ades <MichaelAdes.us@gmail.com>
2023-03-15 19:44:22 -0500

    Adding starter files

---
+++ config.py
@@ -0,0 +1,11 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('DISORD_TOKEN')
+client = discord.Client()
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
